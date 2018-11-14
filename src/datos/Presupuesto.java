bdb76b03bbdb2562f15aeb2ae7e1b2259bba1df9
Roger <rogertim3@gmail.com>
2018-11-14 14:16:14 +0100

    Subida objeto Presupuesto

---
+++ src/datos/Presupuesto.java
@@ -0,0 +1,44 @@
+package datos;
+
+public class Presupuesto {
+
+	private String nombreCliente;
+	private String modelo;
+	private String submodelo;
+	private String accesorios;
+	private String precioTotal;
+	
+
+
+	public Presupuesto(String nombreCliente, String modelo, String submodelo, String accesorios, String precioTotal) {
+		super();
+		this.nombreCliente = nombreCliente;
+		this.modelo = modelo;
+		this.submodelo = submodelo;
+		this.accesorios = accesorios;
+		this.precioTotal = precioTotal;
+	}
+
+	public String getNombreCliente() {
+		return nombreCliente;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public String getSubmodelo() {
+		return submodelo;
+	}
+
+	public String getAccesorios() {
+		return accesorios;
+	}
+
+	public String getPrecioTotal() {
+		return precioTotal;
+	}
+
+	
+	
+}
